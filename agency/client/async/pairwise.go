ed0131262e819d6163092713b0fbc5563197e49a
Harri Lainio <harri.lainio@op.fi>
2020-12-01 19:59:20 +0200

    connection has now a new Label aware interface

--- agency/client/async/pairwise.go
+++ agency/client/async/pairwise.go
@@ -2,9 +2,12 @@ package async
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/findy-network/findy-agent-api/grpc/agency"
+	didexchange "github.com/findy-network/findy-agent/std/didexchange/invitation"
 	"github.com/findy-network/findy-grpc/agency/client"
+	"github.com/lainio/err2"
 )
 
 type Pairwise struct {
@@ -75,4 +78,26 @@ func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Pro
 					Attrs: proofAttrs}}},
 	}
 	return pw.Conn.DoStart(ctx, protocol)
+}
+
+func (pw *Pairwise) Connection(ctx context.Context, invitationJSON string) (pid *agency.ProtocolID, err error) {
+	defer err2.Return(&err)
+
+	// assert that invitation is OK, and we need to return the connection ID
+	// because it's the task id as well
+	var invitation didexchange.Invitation
+	err2.Check(json.Unmarshal([]byte(invitationJSON), &invitation))
+
+	protocol := &agency.Protocol{
+		TypeId: agency.Protocol_CONNECT,
+		Role:   agency.Protocol_INITIATOR,
+		StartMsg: &agency.Protocol_ConnAttr{ConnAttr: &agency.Protocol_Connection{
+			Label:          pw.Label,
+			InvitationJson: invitationJSON,
+		}},
+	}
+	pid, err = pw.Conn.DoStart(ctx, protocol)
+	err2.Check(err)
+	pw.ID = invitation.ID
+	return pid, err
 }
