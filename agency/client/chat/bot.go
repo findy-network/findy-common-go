1bfa9784d707073f29554d45bf1be6bccd92a27c
Harri Lainio <harri.lainio@op.fi>
2020-12-14 17:36:34 +0200

    updated the interface, first load the FSM then start the bot

--- agency/client/chat/bot.go
+++ agency/client/chat/bot.go
@@ -3,6 +3,7 @@ package chat
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -19,15 +20,15 @@ import (
 
 type Bot struct {
 	client.Conn
-	fsm fsm.Machine
+	*fsm.Machine
 }
 
-func (b *Bot) LoadFSM(fName string) (err error) {
+func LoadFSM(fName string, r io.Reader) (m *fsm.Machine, err error) {
 	defer err2.Return(&err)
-	data := err2.Bytes.Try(ioutil.ReadFile(fName))
-	b.fsm = *loadFSMData(fName, data)
-	err2.Check(b.fsm.Initialize())
-	return nil
+	data := err2.Bytes.Try(ioutil.ReadAll(r))
+	m = loadFSMData(fName, data)
+	err2.Check(m.Initialize())
+	return m, nil
 }
 
 func loadFSMData(fName string, data []byte) *fsm.Machine {
@@ -40,9 +41,9 @@ func loadFSMData(fName string, data []byte) *fsm.Machine {
 	return &machine
 }
 
-func (b *Bot) SaveFSM(fName string) (err error) {
+func SaveFSM(m *fsm.Machine, fName string) (err error) {
 	defer err2.Return(&err)
-	data := marshalFSM(fName, &b.fsm)
+	data := marshalFSM(fName, m)
 	err2.Check(ioutil.WriteFile(fName, data, 0644))
 	return nil
 }
@@ -65,15 +66,32 @@ func (b Bot) Run(intCh chan os.Signal) {
 	err2.Check(err)
 
 	// this block is for development without fsm file
-	//b.fsm = EchoMachine //ReqProofMachine //EmailIssuerMachine //
-	b.fsm = EmailIssuerMachine //
-	//b.fsm = ReqProofMachine //EmailIssuerMachine //
-	err2.Check(b.fsm.Initialize())
+	//m := &EchoMachine //ReqProofMachine //EmailIssuerMachine //
+	//err2.Check(SaveFSM(m, "echo-bot.yaml"))
+	//url, err := fsm.GenerateURL("svg", m)
+	//err2.Check(err)
+	//println(url.String())
+	//
+	//m = &EmailIssuerMachine //
+	//err2.Check(SaveFSM(m, "email-issuer-bot.yaml"))
+	//url, err = fsm.GenerateURL("svg", m)
+	//err2.Check(err)
+	//println(url.String())
+	//
+	//m = &ReqProofMachine //EmailIssuerMachine //
+	//err2.Check(SaveFSM(m, "email-verifier-bot.yaml"))
+	//url, err = fsm.GenerateURL("svg", m)
+	//err2.Check(err)
+	//println(url.String())
+	//
+	//b.Machine = m
 
-	// this block is for testing file loading
-	//err2.Check(b.SaveFSM("emailbot.json"))
-	err2.Check(b.SaveFSM("emailbot.yaml"))
-	chat.Machine = &b.fsm
+	err2.Check(b.Machine.Initialize())
+	url, err := fsm.GenerateURL("svg", b.Machine)
+	err2.Check(err)
+	println(url.String())
+
+	chat.Machine = b.Machine
 
 	go chat.Multiplexer(b.Conn)
 
