d01129ef3a01ab1fb7d27b9e0714401f3479d226
Harri Lainio <harri.lainio@op.fi>
2021-04-23 18:27:29 +0300

    gRPC API v1 import and update

--- agency/client/client.go
+++ agency/client/client.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/findy-network/findy-agent-api/grpc/agency"
-	"github.com/findy-network/findy-agent-api/grpc/ops"
+	agency "github.com/findy-network/findy-agent-api/grpc/agency/v1"
+	ops "github.com/findy-network/findy-agent-api/grpc/ops/v1"
 	didexchange "github.com/findy-network/findy-agent/std/didexchange/invitation"
 	"github.com/findy-network/findy-common-go/jwt"
 	"github.com/findy-network/findy-common-go/rpc"
@@ -72,25 +72,31 @@ func OkStatus(s *agency.ProtocolState) bool {
 
 func (pw Pairwise) Issue(ctx context.Context, credDefID, attrsJSON string) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_ISSUE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_ISSUE_CREDENTIAL,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_CredDef{CredDef: &agency.Protocol_Issuing{
-			CredDefId: credDefID,
-			Attrs:     &agency.Protocol_Issuing_AttributesJson{AttributesJson: attrsJSON},
-		}},
+		StartMsg: &agency.Protocol_IssueCredential{
+			IssueCredential: &agency.Protocol_IssueCredentialMsg{
+				CredDefID: credDefID,
+				AttrFmt: &agency.Protocol_IssueCredentialMsg_AttributesJson{
+					AttributesJson: attrsJSON,
+				},
+			},
+		},
 	}
 	return pw.Conn.doRun(ctx, protocol)
 }
 
-func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *agency.Protocol_Attrs) (ch chan *agency.ProtocolState, err error) {
+func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *agency.Protocol_Attributes) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_ISSUE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_ISSUE_CREDENTIAL,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_CredDef{CredDef: &agency.Protocol_Issuing{
-			CredDefId: credDefID,
-			Attrs:     &agency.Protocol_Issuing_Attrs_{Attrs_: attrs},
+		StartMsg: &agency.Protocol_IssueCredential{IssueCredential: &agency.Protocol_IssueCredentialMsg{
+			CredDefID: credDefID,
+			AttrFmt: &agency.Protocol_IssueCredentialMsg_Attributes{
+				Attributes: attrs,
+			},
 		}},
 	}
 	return pw.Conn.doRun(ctx, protocol)
@@ -105,9 +111,9 @@ func (pw *Pairwise) Connection(ctx context.Context, invitationJSON string) (conn
 	err2.Check(json.Unmarshal([]byte(invitationJSON), &invitation))
 
 	protocol := &agency.Protocol{
-		TypeId: agency.Protocol_CONNECT,
+		TypeID: agency.Protocol_DIDEXCHANGE,
 		Role:   agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ConnAttr{ConnAttr: &agency.Protocol_Connection{
+		StartMsg: &agency.Protocol_DIDExchange{DIDExchange: &agency.Protocol_DIDExchangeMsg{
 			Label:          pw.Label,
 			InvitationJson: invitationJSON,
 		}},
@@ -121,8 +127,8 @@ func (pw *Pairwise) Connection(ctx context.Context, invitationJSON string) (conn
 
 func (pw Pairwise) Ping(ctx context.Context) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_TRUST_PING,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_TRUST_PING,
 		Role:         agency.Protocol_INITIATOR,
 	}
 	return pw.Conn.doRun(ctx, protocol)
@@ -130,22 +136,26 @@ func (pw Pairwise) Ping(ctx context.Context) (ch chan *agency.ProtocolState, err
 
 func (pw Pairwise) BasicMessage(ctx context.Context, content string) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_BASIC_MESSAGE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_BASIC_MESSAGE,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg:     &agency.Protocol_BasicMessage{BasicMessage: content},
+		StartMsg: &agency.Protocol_BasicMessage{
+			BasicMessage: &agency.Protocol_BasicMessageMsg{
+				Text: content,
+			},
+		},
 	}
 	return pw.Conn.doRun(ctx, protocol)
 }
 
 func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_PROOF,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_PRESENT_PROOF,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ProofReq{
-			ProofReq: &agency.Protocol_ProofRequest{
-				AttrFmt: &agency.Protocol_ProofRequest_AttributesJson{
+		StartMsg: &agency.Protocol_PresentProof{
+			PresentProof: &agency.Protocol_PresentProofMsg{
+				AttrFmt: &agency.Protocol_PresentProofMsg_AttributesJson{
 					AttributesJson: proofAttrs}}},
 	}
 	return pw.Conn.doRun(ctx, protocol)
@@ -153,13 +163,13 @@ func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (ch chan *ag
 
 func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Protocol_Proof) (ch chan *agency.ProtocolState, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_PROOF,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_PRESENT_PROOF,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ProofReq{
-			ProofReq: &agency.Protocol_ProofRequest{
-				AttrFmt: &agency.Protocol_ProofRequest_Attrs{
-					Attrs: proofAttrs}}},
+		StartMsg: &agency.Protocol_PresentProof{
+			PresentProof: &agency.Protocol_PresentProofMsg{
+				AttrFmt: &agency.Protocol_PresentProofMsg_Attributes{
+					Attributes: proofAttrs}}},
 	}
 	return pw.Conn.doRun(ctx, protocol)
 }
@@ -167,12 +177,12 @@ func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Pro
 func (conn Conn) Listen(ctx context.Context, protocol *agency.ClientID, cOpts ...grpc.CallOption) (ch chan *agency.AgentStatus, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewAgentClient(conn)
+	c := agency.NewAgentServiceClient(conn)
 	statusCh := make(chan *agency.AgentStatus)
 
 	stream, err := c.Listen(ctx, protocol, cOpts...)
 	err2.Check(err)
-	glog.V(3).Infoln("successful start of listen id:", protocol.Id)
+	glog.V(3).Infoln("successful start of listen id:", protocol.ID)
 	go func() {
 		defer err2.CatchTrace(func(err error) {
 			glog.V(1).Infoln("WARNING: error when reading response:", err)
@@ -195,10 +205,10 @@ func (conn Conn) Listen(ctx context.Context, protocol *agency.ClientID, cOpts ..
 func (conn Conn) PSMHook(ctx context.Context, cOpts ...grpc.CallOption) (ch chan *ops.AgencyStatus, err error) {
 	defer err2.Return(&err)
 
-	opsClient := ops.NewAgencyClient(conn)
+	opsClient := ops.NewAgencyServiceClient(conn)
 	statusCh := make(chan *ops.AgencyStatus)
 
-	stream, err := opsClient.PSMHook(ctx, &ops.DataHook{Id: utils.UUID()}, cOpts...)
+	stream, err := opsClient.PSMHook(ctx, &ops.DataHook{ID: utils.UUID()}, cOpts...)
 	err2.Check(err)
 	glog.V(3).Infoln("successful start of listen PSM hook id:")
 	go func() {
@@ -223,12 +233,12 @@ func (conn Conn) PSMHook(ctx context.Context, cOpts ...grpc.CallOption) (ch chan
 func (conn Conn) doRun(ctx context.Context, protocol *agency.Protocol) (ch chan *agency.ProtocolState, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(conn)
+	c := agency.NewProtocolServiceClient(conn)
 	statusCh := make(chan *agency.ProtocolState)
 
 	stream, err := c.Run(ctx, protocol)
 	err2.Check(err)
-	glog.V(3).Infoln("successful start of:", protocol.TypeId)
+	glog.V(3).Infoln("successful start of:", protocol.TypeID)
 	go func() {
 		defer err2.CatchTrace(func(err error) {
 			glog.V(3).Infoln("err when reading response", err)
@@ -251,43 +261,43 @@ func (conn Conn) doRun(ctx context.Context, protocol *agency.Protocol) (ch chan 
 func (conn Conn) DoStart(ctx context.Context, protocol *agency.Protocol, cOpts ...grpc.CallOption) (pid *agency.ProtocolID, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(conn)
+	c := agency.NewProtocolServiceClient(conn)
 	pid, err = c.Start(ctx, protocol, cOpts...)
 	err2.Check(err)
 
-	glog.V(3).Infoln("successful start of:", protocol.TypeId)
+	glog.V(3).Infoln("successful start of:", protocol.TypeID)
 	return pid, nil
 }
 
 func (conn Conn) DoResume(ctx context.Context, state *agency.ProtocolState, cOpts ...grpc.CallOption) (pid *agency.ProtocolID, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(conn)
+	c := agency.NewProtocolServiceClient(conn)
 	pid, err = c.Resume(ctx, state, cOpts...)
 	err2.Check(err)
 
-	glog.V(3).Infoln("successful resume of:", state.ProtocolId.TypeId)
+	glog.V(3).Infoln("successful resume of:", state.ProtocolID.TypeID)
 	return pid, nil
 }
 
 func (conn Conn) DoRelease(ctx context.Context, id *agency.ProtocolID, cOpts ...grpc.CallOption) (pid *agency.ProtocolID, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(conn)
+	c := agency.NewProtocolServiceClient(conn)
 	pid, err = c.Release(ctx, id, cOpts...)
 	err2.Check(err)
 
-	glog.V(3).Infoln("successful release of:", id.TypeId)
+	glog.V(3).Infoln("successful release of:", id.TypeID)
 	return pid, nil
 }
 
 func (conn Conn) DoStatus(ctx context.Context, id *agency.ProtocolID, cOpts ...grpc.CallOption) (status *agency.ProtocolStatus, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(conn)
+	c := agency.NewProtocolServiceClient(conn)
 	status, err = c.Status(ctx, id, cOpts...)
 	err2.Check(err)
 
-	glog.V(3).Infoln("successful status of:", id.TypeId)
+	glog.V(3).Infoln("successful status of:", id.TypeID)
 	return status, nil
 }
