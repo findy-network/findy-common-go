cfe4a1b3deda636108d3eeb5a0b0df9657109794
Harri Lainio <harri.lainio@op.fi>
2021-04-23 18:26:43 +0300

    gRPC API v1 import and update

--- agency/client/async/pairwise.go
+++ agency/client/async/pairwise.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/findy-network/findy-agent-api/grpc/agency"
+	agency "github.com/findy-network/findy-agent-api/grpc/agency/v1"
 	didexchange "github.com/findy-network/findy-agent/std/didexchange/invitation"
 	"github.com/findy-network/findy-common-go/agency/client"
 	"github.com/lainio/err2"
@@ -22,35 +22,45 @@ func NewPairwise(conn client.Conn, ID string, cOpts ...grpc.CallOption) *Pairwis
 
 func (pw Pairwise) BasicMessage(ctx context.Context, content string) (pid *agency.ProtocolID, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_BASIC_MESSAGE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_BASIC_MESSAGE,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg:     &agency.Protocol_BasicMessage{BasicMessage: content},
+		StartMsg: &agency.Protocol_BasicMessage{
+			BasicMessage: &agency.Protocol_BasicMessageMsg{
+				Text: content,
+			},
+		},
 	}
 	return pw.Conn.DoStart(ctx, protocol, pw.cOpts...)
 }
 
 func (pw Pairwise) Issue(ctx context.Context, credDefID, attrsJSON string) (pid *agency.ProtocolID, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_ISSUE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_ISSUE_CREDENTIAL,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_CredDef{CredDef: &agency.Protocol_Issuing{
-			CredDefId: credDefID,
-			Attrs:     &agency.Protocol_Issuing_AttributesJson{AttributesJson: attrsJSON},
-		}},
+		StartMsg: &agency.Protocol_IssueCredential{
+			IssueCredential: &agency.Protocol_IssueCredentialMsg{
+				CredDefID: credDefID,
+				AttrFmt: &agency.Protocol_IssueCredentialMsg_AttributesJson{
+					AttributesJson: attrsJSON,
+				},
+			},
+		},
 	}
 	return pw.Conn.DoStart(ctx, protocol, pw.cOpts...)
 }
 
-func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *agency.Protocol_Attrs) (pid *agency.ProtocolID, err error) {
+func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *agency.Protocol_Attributes) (pid *agency.ProtocolID, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_ISSUE,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_ISSUE_CREDENTIAL,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_CredDef{CredDef: &agency.Protocol_Issuing{
-			CredDefId: credDefID,
-			Attrs:     &agency.Protocol_Issuing_Attrs_{Attrs_: attrs},
+		StartMsg: &agency.Protocol_IssueCredential{IssueCredential: &agency.Protocol_IssueCredentialMsg{
+			CredDefID: credDefID,
+			AttrFmt: &agency.Protocol_IssueCredentialMsg_Attributes{
+				Attributes: attrs,
+			},
 		}},
 	}
 	return pw.Conn.DoStart(ctx, protocol, pw.cOpts...)
@@ -58,12 +68,12 @@ func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *
 
 func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (pid *agency.ProtocolID, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_PROOF,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_PRESENT_PROOF,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ProofReq{
-			ProofReq: &agency.Protocol_ProofRequest{
-				AttrFmt: &agency.Protocol_ProofRequest_AttributesJson{
+		StartMsg: &agency.Protocol_PresentProof{
+			PresentProof: &agency.Protocol_PresentProofMsg{
+				AttrFmt: &agency.Protocol_PresentProofMsg_AttributesJson{
 					AttributesJson: proofAttrs}}},
 	}
 	return pw.Conn.DoStart(ctx, protocol, pw.cOpts...)
@@ -71,13 +81,13 @@ func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (pid *agency
 
 func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Protocol_Proof) (pid *agency.ProtocolID, err error) {
 	protocol := &agency.Protocol{
-		ConnectionId: pw.ID,
-		TypeId:       agency.Protocol_PROOF,
+		ConnectionID: pw.ID,
+		TypeID:       agency.Protocol_PRESENT_PROOF,
 		Role:         agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ProofReq{
-			ProofReq: &agency.Protocol_ProofRequest{
-				AttrFmt: &agency.Protocol_ProofRequest_Attrs{
-					Attrs: proofAttrs}}},
+		StartMsg: &agency.Protocol_PresentProof{
+			PresentProof: &agency.Protocol_PresentProofMsg{
+				AttrFmt: &agency.Protocol_PresentProofMsg_Attributes{
+					Attributes: proofAttrs}}},
 	}
 	return pw.Conn.DoStart(ctx, protocol, pw.cOpts...)
 }
@@ -91,9 +101,9 @@ func (pw *Pairwise) Connection(ctx context.Context, invitationJSON string) (pid 
 	err2.Check(json.Unmarshal([]byte(invitationJSON), &invitation))
 
 	protocol := &agency.Protocol{
-		TypeId: agency.Protocol_CONNECT,
+		TypeID: agency.Protocol_DIDEXCHANGE,
 		Role:   agency.Protocol_INITIATOR,
-		StartMsg: &agency.Protocol_ConnAttr{ConnAttr: &agency.Protocol_Connection{
+		StartMsg: &agency.Protocol_DIDExchange{DIDExchange: &agency.Protocol_DIDExchangeMsg{
 			Label:          pw.Label,
 			InvitationJson: invitationJSON,
 		}},
@@ -111,10 +121,10 @@ func (pw *Pairwise) Resume(
 	protocolState agency.ProtocolState_State,
 ) (pid *agency.ProtocolID, err error) {
 	state := &agency.ProtocolState{
-		ProtocolId: &agency.ProtocolID{
-			TypeId: protocol,
-			Role:   agency.Protocol_RESUME,
-			Id:     id,
+		ProtocolID: &agency.ProtocolID{
+			TypeID: protocol,
+			Role:   agency.Protocol_RESUMER,
+			ID:     id,
 		},
 		State: protocolState,
 	}
