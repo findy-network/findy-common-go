e7ab8ef3630b3b38ee98d615471a228dad563baf
Harri Lainio <harri.lainio@op.fi>
2021-10-08 15:33:41 +0300

    adding support for multible DB instances

--- crypto/db/db.go
+++ crypto/db/db.go
@@ -1,3 +1,7 @@
+// package db implements Bolt based database which can be encrypted on the fly
+// and which supports automatic backups. It offers very simple API and hides all
+// the complex stuff behind it. It's thread safe. More information see the Cfg
+// struct.
 package db
 
 import (
@@ -11,12 +15,14 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
+// Cfg is configuration needed to create and open managed database.
 type Cfg struct {
-	Filename   string
-	BackupName string
-	Buckets    [][]byte
+	Filename   string   // Filename is full path file name of the DB file
+	BackupName string   // Base part of the backup file names. Date and time is added.
+	Buckets    [][]byte // Buckets is list of the buckets needed
 }
 
+// Mgd is a managed and encrypted (option, can be pre-procession as well) DB.
 type Mgd struct {
 	Cfg
 	db    *bolt.DB
@@ -24,6 +30,9 @@ type Mgd struct {
 	l     sync.Mutex
 }
 
+// operate is a key element of the managed Bolt DB. It keeps track of closing
+// and opening of the DB which is needed that DB can operate and backups can be
+// taken without explicitly closing the database.
 func (m *Mgd) operate(f func(db *bolt.DB) error) (err error) {
 	defer err2.Annotate("db operate", &err)
 
@@ -34,12 +43,24 @@ func (m *Mgd) operate(f func(db *bolt.DB) error) (err error) {
 	if m.db == nil {
 		err2.Check(m.open())
 	}
-	return f(mgedDB.db)
+	return f(m.db)
 }
 
 var (
 	mgedDB Mgd
 )
+
+// New creates a new managed and encrypted database. This is a preferred way to
+// use the managed database package. There is also the alternated Init function when you
+// don't need to store the Mgd instance by yourself. It's for the cases when
+// only one managed database is needed per a process or an application. Database
+// is ready to use after this call. You don't need to open it and backup can be
+// taken during the run. See more information of Cfg struct.
+func New(cfg Cfg) *Mgd {
+	return &Mgd{
+		Cfg: cfg,
+	}
+}
 
 // Init initializes managed version of the encrypted database. Database is ready
 // to use after this call. See more information of Cfg struct.
@@ -120,8 +141,8 @@ func (m *Mgd) backupName() string {
 
 // AddKeyValueToBucket add value to bucket pointed by the index. keyValue and
 // index use Data type's operators to encrypt and hash data on the fly.
-func AddKeyValueToBucket(bucket []byte, keyValue, index *Data) (err error) {
-	return mgedDB.operate(func(DB *bolt.DB) error {
+func (db *Mgd) AddKeyValueToBucket(bucket []byte, keyValue, index *Data) (err error) {
+	return db.operate(func(DB *bolt.DB) error {
 		defer err2.Annotate("add key", &err)
 
 		err2.Check(DB.Update(func(tx *bolt.Tx) (err error) {
@@ -135,10 +156,16 @@ func AddKeyValueToBucket(bucket []byte, keyValue, index *Data) (err error) {
 	})
 }
 
+// AddKeyValueToBucket add value to bucket pointed by the index. keyValue and
+// index use Data type's operators to encrypt and hash data on the fly.
+func AddKeyValueToBucket(bucket []byte, keyValue, index *Data) (err error) {
+	return mgedDB.AddKeyValueToBucket(bucket, keyValue, index)
+}
+
 // RmKeyValueFromBucket removes value pointed by the index from the bucket.
 // The index uses Data type's operators to encrypt and hash data on the fly.
-func RmKeyValueFromBucket(bucket []byte, index *Data) (err error) {
-	return mgedDB.operate(func(DB *bolt.DB) error {
+func (db *Mgd) RmKeyValueFromBucket(bucket []byte, index *Data) (err error) {
+	return db.operate(func(DB *bolt.DB) error {
 		defer err2.Annotate("rm key", &err)
 
 		err2.Check(DB.Update(func(tx *bolt.Tx) (err error) {
@@ -152,13 +179,38 @@ func RmKeyValueFromBucket(bucket []byte, index *Data) (err error) {
 	})
 }
 
+// RmKeyValueFromBucket removes value pointed by the index from the bucket.
+// The index uses Data type's operators to encrypt and hash data on the fly.
+func RmKeyValueFromBucket(bucket []byte, index *Data) (err error) {
+	return mgedDB.RmKeyValueFromBucket(bucket, index)
+}
+
 // GetKeyValueFromBucket writes keyValue data by the index from a bucket. It
-// returns found if key value exists. Returns are only if it cannot perform the
-// transaction successfully.
-func GetKeyValueFromBucket(bucket []byte, index, keyValue *Data) (found bool, err error) {
+// returns `found` if key value exists. Errors will return only if it cannot
+// perform the transaction successfully.
+func GetKeyValueFromBucket(
+	bucket []byte,
+	index, keyValue *Data,
+) (
+	found bool,
+	err error,
+) {
+	return mgedDB.GetKeyValueFromBucket(bucket, index, keyValue)
+}
+
+// GetKeyValueFromBucket writes keyValue data by the index from a bucket. It
+// returns `found` if key value exists. Errors will return only if it cannot
+// perform the transaction successfully.
+func (db *Mgd) GetKeyValueFromBucket(
+	bucket []byte,
+	index, keyValue *Data,
+) (
+	found bool,
+	err error,
+) {
 	defer err2.Annotate("get value", &err)
 
-	err2.Check(mgedDB.operate(func(DB *bolt.DB) error {
+	err2.Check(db.operate(func(DB *bolt.DB) error {
 		err2.Check(DB.View(func(tx *bolt.Tx) (err error) {
 			defer err2.Return(&err)
 
@@ -181,6 +233,13 @@ func GetKeyValueFromBucket(bucket []byte, index, keyValue *Data) (found bool, er
 // file specified by the interval. Ticker can be stopped with returned done
 // channel.
 func BackupTicker(interval time.Duration) (done chan<- struct{}) {
+	return mgedDB.BackupTicker(interval)
+}
+
+// BackupTicker creates a backup ticker which takes backup copy of the database
+// file specified by the interval. Ticker can be stopped with returned done
+// channel.
+func (db *Mgd) BackupTicker(interval time.Duration) (done chan<- struct{}) {
 	ticker := time.NewTicker(interval)
 	doneCh := make(chan struct{})
 	go func() {
@@ -192,7 +251,7 @@ func BackupTicker(interval time.Duration) (done chan<- struct{}) {
 			case <-doneCh:
 				return
 			case <-ticker.C:
-				_, err := Backup()
+				_, err := db.Backup()
 				if err != nil {
 					glog.Errorln("backup ticker:", err)
 				}
@@ -203,54 +262,71 @@ func BackupTicker(interval time.Duration) (done chan<- struct{}) {
 }
 
 // Backup takes backup copy of the database. Before backup the database is
-// closed.
+// closed automatically and only dirty databases are backed up.
 func Backup() (did bool, err error) {
+	return mgedDB.Backup()
+}
+
+// Backup takes backup copy of the database. Before backup the database is
+// closed automatically and only dirty databases are backed up.
+func (db *Mgd) Backup() (did bool, err error) {
 	defer err2.Annotate("backup", &err)
 
-	mgedDB.l.Lock()
-	defer mgedDB.l.Unlock()
+	db.l.Lock()
+	defer db.l.Unlock()
 
-	if !mgedDB.dirty {
+	if !db.dirty {
 		glog.V(1).Infoln("db isn't dirty, skipping backup")
 		return false, nil
 	}
-	if mgedDB.db != nil {
-		err2.Check(mgedDB.close())
+	if db.db != nil {
+		err2.Check(db.close())
 	}
 
 	// we keep locks on during the whole copy, but try to do it as fast as
 	// possible. If this would be critical we could first read the source file
 	// when locks are on and then write the target file in a new gorountine.
-	backupName := mgedDB.backupName()
-	err2.Check(backup.FileCopy(mgedDB.Filename, backupName))
+	backupName := db.backupName()
+	err2.Check(backup.FileCopy(db.Filename, backupName))
 	glog.V(1).Infoln("successful backup to file:", backupName)
 
-	mgedDB.dirty = false
+	db.dirty = false
 	return true, nil
 }
 
 // Wipe removes the whole database and its master file.
 func Wipe() (err error) {
+	return mgedDB.Wipe()
+}
+
+// Wipe removes the whole database and its master file.
+func (db *Mgd) Wipe() (err error) {
 	defer err2.Annotate("wipe", &err)
 
-	mgedDB.l.Lock()
-	defer mgedDB.l.Unlock()
+	db.l.Lock()
+	defer db.l.Unlock()
 
-	if mgedDB.db != nil {
-		err2.Check(mgedDB.close())
+	if db.db != nil {
+		err2.Check(db.close())
 	}
 
-	return os.RemoveAll(mgedDB.Filename)
+	return os.RemoveAll(db.Filename)
 }
 
 // Close closes the database. It can be used after that if wanted. Transactions
 // opens the database when needed.
 func Close() (err error) {
-	mgedDB.l.Lock()
-	defer mgedDB.l.Unlock()
+	return mgedDB.close()
+}
 
-	if mgedDB.db != nil {
-		return mgedDB.close()
+// Close closes the database. It can be used after that if wanted. Transactions
+// opens the database when needed.
+func (db *Mgd) Close() (err error) {
+	db.l.Lock()
+	defer db.l.Unlock()
+
+	if db.db != nil {
+		return db.close()
 	}
 
 	return nil
