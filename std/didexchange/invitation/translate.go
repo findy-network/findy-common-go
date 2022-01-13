f397922b9b5da39368f704f347899fb227c4746b
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-01-13 11:13:00 +0200

    Support both b64 url encodings when translating invitation.

--- std/didexchange/invitation/translate.go
+++ std/didexchange/invitation/translate.go
@@ -10,6 +10,14 @@ import (
 
 const prefix = "didcomm://aries_connection_invitation?c_i="
 
+func decodeB64(str string) ([]byte, error) {
+	data, err := base64.URLEncoding.DecodeString(str)
+	if err != nil {
+		data, err = base64.RawURLEncoding.DecodeString(str)
+	}
+	return data, err
+}
+
 func Translate(s string) (i Invitation, err error) {
 	defer err2.Annotate("invitation translate", &err)
 
@@ -25,7 +33,7 @@ func Translate(s string) (i Invitation, err error) {
 	err2.Check(err)
 
 	raw := m["c_i"][0]
-	d := err2.Bytes.Try(base64.RawURLEncoding.DecodeString(raw))
+	d := err2.Bytes.Try(decodeB64(raw))
 	err2.Check(json.Unmarshal(d, &i))
 
 	return i, nil
