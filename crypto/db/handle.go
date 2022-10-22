862598e909eac8631b5447dd2a45aa125478c761
Harri Lainio <harri.lainio@iki.fi>
2022-10-22 11:58:14 +0300

    format Handle code for interface

--- crypto/db/handle.go
+++ crypto/db/handle.go
@@ -5,20 +5,8 @@ import "time"
 type Handle interface {
 	AddKeyValueToBucket(bucket []byte, keyValue, index *Data) (err error)
 	RmKeyValueFromBucket(bucket []byte, index *Data) (err error)
-	GetKeyValueFromBucket(
-		bucket []byte,
-		index, keyValue *Data,
-	) (
-		found bool,
-		err error,
-	)
-	GetAllValuesFromBucket(
-		bucket []byte,
-		transforms ...Filter,
-	) (
-		values [][]byte,
-		err error,
-	)
+	GetKeyValueFromBucket(bucket []byte, index, keyValue *Data) (found bool, err error)
+	GetAllValuesFromBucket(bucket []byte, transforms ...Filter) (values [][]byte, err error)
 	BackupTicker(interval time.Duration) (done chan<- struct{})
 	Backup() (did bool, err error)
 	Wipe() (err error)
