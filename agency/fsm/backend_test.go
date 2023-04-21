9e29e45cf5eff3ca83f92cecebe0aff57ea7c7e8
Harri Lainio <harri.lainio@iki.fi>
2023-04-21 10:57:26 +0300

    Backend service FSM tests finalizing

--- agency/fsm/backend_test.go
+++ agency/fsm/backend_test.go
@@ -8,6 +8,46 @@ import (
 	"github.com/lainio/err2/assert"
 )
 
+func TestBackendSimple(t *testing.T) {
+	type args struct {
+		m       *Machine
+		content string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantStr string
+		want    bool
+	}{
+		{"simple", args{&simpleBackendMachine, "TEST"}, "she says: TEST", true},
+		{"simple_not", args{&simpleBackendMachine, "not"}, "not", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.PushTester(t)
+			defer assert.PopTester()
+			defer err2.Catch(func(err error) { assert.NoError(err) })
+
+			err := tt.args.m.Initialize()
+			assert.NoError(err)
+			tt.args.m.InitLua()
+			assert.Equal(tt.args.m.Type, MachineTypeBackend)
+			beData := newBackend(tt.args.content)
+			transition := tt.args.m.TriggersByBackendData(beData)
+			assert.NotNil(transition)
+			o := transition.BuildSendEventsFromBackendData(beData)
+			assert.INotNil(o)
+			assert.SLen(o, 1)
+			assert.NotNil(o[0].EventData.Backend)
+			if tt.want {
+				assert.Equal(o[0].EventData.Backend.Content, tt.wantStr)
+			} else {
+				assert.NotEqual(o[0].EventData.Backend.Content, tt.wantStr)
+			}
+		})
+	}
+}
+
 func TestBackendTestLuaTrigger(t *testing.T) {
 	type args struct {
 		m       *Machine
@@ -74,6 +114,34 @@ local i=getRegValue("MEM", "INPUT")
 local retval=i .. "+" .. i
 setRegValue("MEM", "OUTPUT", retval)
  `
+	simpleBackendMachine = Machine{
+		Name: "simple backend machine",
+		Type: MachineTypeBackend,
+		Initial: &Transition{
+			Target: "IDLE",
+		},
+		States: map[string]*State{
+			"IDLE": {
+				Transitions: []*Transition{
+					{
+						Trigger: &Event{
+							Protocol: "backend",
+							Rule:     "INPUT",
+						},
+						Sends: []*Event{{
+							Protocol: "backend",
+							Rule:     "FORMAT",
+							Data:     "she says: %s",
+						}},
+						Target: "IDLE",
+					},
+				},
+			},
+			"TERMINATE": {
+				Terminate: true,
+			},
+		},
+	}
 
 	luaBackendMachine = Machine{
 		Name: "luaBackend_machine",
